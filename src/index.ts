bd693b47b9cfa13f36d228abfa95e6e270426adb
Charles DuBose <kayakyakr@gmail.com>
2025-04-07 17:52:23 -0400

    Convert to cloudrun function

--- src/index.ts
+++ src/index.ts
@@ -1,187 +1,169 @@
-import { Hono } from "hono";
-import { App, Octokit } from "octokit";
-import { createWebMiddleware } from "@octokit/webhooks";
-import Docker from "dockerode";
+import { App } from "octokit";
+import { createNodeMiddleware } from "@octokit/webhooks";
 import kebabCase from "kebab-case";
 import { ReviewAndComments, reviewAndComments } from "./queries";
+import { JobsClient } from "@google-cloud/run";
+import dotenv from "dotenv";
 
-type Variables = {
-  octokit: Octokit;
-};
+dotenv.config();
 
-const app = new Hono<{ Bindings: CloudflareBindings; Variables: Variables }>();
+const octoApp = new App({
+  appId: process.env.APP_ID ?? "",
+  privateKey: process.env.PRIVATE_KEY ?? "",
+  webhooks: {
+    secret: process.env.WEBHOOK_SECRET ?? "",
+  },
+});
 
-app.use(async (context, next) => {
-  const docker = new Docker({ socketPath: "/var/run/docker.sock" });
-  const octoApp = new App({
-    appId: context.env.APP_ID,
-    privateKey: context.env.PRIVATE_KEY,
-    webhooks: {
-      secret: context.env.WEBHOOK_SECRET,
-    },
-  });
+octoApp.webhooks.on("issues.labeled", async ({ payload, octokit }) => {
+  if (!payload.installation) return;
 
-  octoApp.webhooks.on("issues.labeled", async ({ payload, octokit }) => {
-    if (!payload.installation) return;
+  if (payload.label?.name === "aider") {
+    await octokit.rest.issues.createComment({
+      owner: payload.repository.owner.login,
+      repo: payload.repository.name,
+      issue_number: payload.issue.number,
+      body: "I'm on it!",
+    });
 
-    if (payload.label?.name === "aider") {
-      await octokit.rest.issues.createComment({
+    const title = `${payload.issue.title}`;
+    const body = `${payload.issue.body}`;
+
+    const branchName = `ai-jr-dev/${payload.issue.number}${kebabCase(
+      payload.issue.title
+    )}`;
+
+    try {
+      await octokit.rest.repos.getBranch({
+        repo: payload.repository.name,
+        owner: payload.repository.owner.login,
+        branch: branchName,
+      });
+    } catch {
+      const defaultBranch = await octokit.rest.repos.getBranch({
+        repo: payload.repository.name,
+        owner: payload.repository.owner.login,
+        branch: payload.repository.default_branch,
+      });
+      await octokit.rest.git.createRef({
+        repo: payload.repository.name,
+        owner: payload.repository.owner.login,
+        sha: defaultBranch.data.commit.sha,
+        ref: `refs/heads/${branchName}`,
+      });
+    }
+
+    try {
+      // TODO: clean user input
+      const auth = await octokit.rest.apps.createInstallationAccessToken({
+        installation_id: payload.installation.id,
+      });
+      const prompt = `Apply all necessary changes based on below issue description. \nIssue title: ${title}\nIssue description:\n${body}`;
+
+      const jobsClient = new JobsClient();
+      const [operation] = await jobsClient.runJob({
+        name: "aider-runner",
+        overrides: {
+          containerOverrides: [
+            {
+              env: [
+                { name: "AIDER_ARGS", value: `--message "${prompt}"` },
+                {
+                  name: "REPO_NAME",
+                  value: `https://x-access-token:${
+                    auth.data.token
+                  }@${payload.repository.clone_url.slice(8)}`,
+                },
+                { name: "BRANCH_NAME", value: branchName },
+              ],
+            },
+          ],
+        },
+      });
+      const [_response] = await operation.promise();
+      // TODO: use image output to generate a PR summary, including any commands the user needs to run for the AI
+      await octokit.rest.pulls.create({
         owner: payload.repository.owner.login,
         repo: payload.repository.name,
-        issue_number: payload.issue.number,
-        body: "I'm on it!",
+        title: `[AI] ${payload.issue.title}`,
+        head: branchName,
+        base: payload.repository.default_branch,
       });
-
-      const title = `${payload.issue.title}`;
-      const body = `${payload.issue.body}`;
-
-      const branchName = `ai-jr-dev/${payload.issue.number}${kebabCase(
-        payload.issue.title
-      )}`;
-
-      try {
-        await octokit.rest.repos.getBranch({
-          repo: payload.repository.name,
-          owner: payload.repository.owner.login,
-          branch: branchName,
-        });
-      } catch {
-        const defaultBranch = await octokit.rest.repos.getBranch({
-          repo: payload.repository.name,
-          owner: payload.repository.owner.login,
-          branch: payload.repository.default_branch,
-        });
-        await octokit.rest.git.createRef({
-          repo: payload.repository.name,
-          owner: payload.repository.owner.login,
-          sha: defaultBranch.data.commit.sha,
-          ref: `refs/heads/${branchName}`,
-        });
-      }
-
-      try {
-        // TODO: clean user input
-        const auth = await octokit.rest.apps.createInstallationAccessToken({
-          installation_id: payload.installation.id,
-        });
-        const prompt = `Apply all necessary changes based on below issue description. \nIssue title: ${title}\nIssue description:\n${body}`;
-
-        // TODO: Extract to cloud-image
-        const [_, container] = await docker.run(
-          "aider-runner",
-          [],
-          process.stdout,
-          {
-            Env: [
-              `OPENROUTER_API_KEY=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`,
-              `MODEL=openrouter/google/gemini-2.0-flash-001`, //openrouter/google/gemini-2.5-pro-exp-03-25:free`,
-              //`EDITOR_MODEL=openrouter/open-r1/olympiccoder-32b:free`,
-              //`WEAK_MODEL=openrouter/open-r1/olympiccoder-32b:free`,
-              `AIDER_ARGS=--message "${prompt}"`,
-              `REPO_NAME=https://x-access-token:${
-                auth.data.token
-              }@${payload.repository.clone_url.slice(8)}`,
-              `BRANCH_NAME=${branchName}`,
-            ],
-          }
-        );
-        container.remove();
-        // TODO: use image output to generate a PR summary, including any commands the user needs to run for the AI
-        await octokit.rest.pulls.create({
-          owner: payload.repository.owner.login,
-          repo: payload.repository.name,
-          title: `[AI] ${payload.issue.title}`,
-          head: branchName,
-          base: payload.repository.default_branch,
-        });
-      } catch (e: any) {
-        console.error(e);
-      }
+    } catch (e: any) {
+      console.error(e);
     }
-  });
-
-  octoApp.webhooks.on(
-    "pull_request_review.submitted",
-    async ({ payload, octokit }) => {
-      if (!payload.installation) return;
-      if (
-        payload.pull_request.user?.id === Number(context.env.APP_USER_ID) &&
-        payload.review.state === "changes_requested"
-      ) {
-        const resp = await octokit.graphql<ReviewAndComments>(
-          reviewAndComments,
-          {
-            owner: payload.repository.owner.login,
-            repo: payload.repository.name,
-            pr: payload.pull_request.number,
-          }
-        );
-        const comments = resp.repository.pullRequest.reviews.nodes
-          .find((review) => review.id === payload.review.node_id)
-          ?.comments.nodes.map((comment, i) => {
-            const commentString: string[] = [];
-            commentString.push(`${i}. files: ${comment.path}`);
-            if (comment.line) {
-              commentString.push(
-                comment.startLine
-                  ? `lines: ${comment.startLine} - ${comment.line}`
-                  : `line: ${comment.line}`
-              );
-            }
-            commentString.push(comment.bodyText);
-            return commentString.join("; ");
-          })
-          .join("\n");
-        // put together new prompt
-        const prompt = `Apply all necessary changes based on below issue description. Related to the all files: ${payload.review.body}\nSpecific Files: ${comments}`;
-        const auth = await octokit.rest.apps.createInstallationAccessToken({
-          installation_id: payload.installation.id,
-        });
-        // send prompt to aider
-        // TODO: Extract to cloud-image
-        const [_, container] = await docker.run(
-          "aider-runner",
-          [],
-          process.stdout,
-          {
-            Env: [
-              `OPENROUTER_API_KEY=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`,
-              `MODEL=openrouter/google/gemini-2.0-flash-001`, //openrouter/google/gemini-2.5-pro-exp-03-25:free`,
-              //`EDITOR_MODEL=openrouter/open-r1/olympiccoder-32b:free`,
-              //`WEAK_MODEL=openrouter/open-r1/olympiccoder-32b:free`,
-              `AIDER_ARGS=--message "${prompt}"`,
-              `REPO_NAME=https://x-access-token:${
-                auth.data.token
-              }@${payload.repository.clone_url.slice(8)}`,
-              `BRANCH_NAME=${payload.pull_request.head.ref}`,
-            ],
-          }
-        );
-        container.remove();
-        // TODO: use image output to make any comments, such as commands that the AI needs the user's help running
-        // TODO: clean up - use graphql API to hide all change requests
-        // TODO: Mark any floating comments as resolved.
-        // reset review
-        octokit.rest.pulls.requestReviewers({
-          owner: payload.repository.owner.login,
-          repo: payload.repository.name,
-          pull_number: payload.pull_request.number,
-          reviewers: [payload.review.user?.login || ""],
-        });
-      }
-    }
-  );
-
-  const middleware = createWebMiddleware(octoApp.webhooks, { path: "/" });
-  const resp = await middleware(context.req.raw);
-
-  if (resp.ok) return resp;
-
-  next();
+  }
 });
 
-app.get("/", (c) => {
-  return c.text("ok");
-});
+octoApp.webhooks.on(
+  "pull_request_review.submitted",
+  async ({ payload, octokit }) => {
+    if (!payload.installation) return;
+    if (
+      payload.pull_request.user?.id === Number(process.env.APP_USER_ID) &&
+      payload.review.state === "changes_requested"
+    ) {
+      const resp = await octokit.graphql<ReviewAndComments>(reviewAndComments, {
+        owner: payload.repository.owner.login,
+        repo: payload.repository.name,
+        pr: payload.pull_request.number,
+      });
+      const comments = resp.repository.pullRequest.reviews.nodes
+        .find((review) => review.id === payload.review.node_id)
+        ?.comments.nodes.map((comment, i) => {
+          const commentString: string[] = [];
+          commentString.push(`${i}. files: ${comment.path}`);
+          if (comment.line) {
+            commentString.push(
+              comment.startLine
+                ? `lines: ${comment.startLine} - ${comment.line}`
+                : `line: ${comment.line}`
+            );
+          }
+          commentString.push(comment.bodyText);
+          return commentString.join("; ");
+        })
+        .join("\n");
+      // put together new prompt
+      const prompt = `Apply all necessary changes based on below issue description. Related to the all files: ${payload.review.body}\nSpecific Files: ${comments}`;
+      const auth = await octokit.rest.apps.createInstallationAccessToken({
+        installation_id: payload.installation.id,
+      });
+      // send prompt to aider
+      const jobsClient = new JobsClient();
+      const [operation] = await jobsClient.runJob({
+        name: "aider-runner",
+        overrides: {
+          containerOverrides: [
+            {
+              env: [
+                { name: "AIDER_ARGS", value: `--message "${prompt}"` },
+                {
+                  name: "REPO_NAME",
+                  value: `https://x-access-token:${
+                    auth.data.token
+                  }@${payload.repository.clone_url.slice(8)}`,
+                },
+                { name: "BRANCH_NAME", value: payload.pull_request.head.ref },
+              ],
+            },
+          ],
+        },
+      });
+      const [_response] = await operation.promise();
 
-export default app;
+      // TODO: use image output to make any comments, such as commands that the AI needs the user's help running
+      // TODO: clean up - use graphql API to hide all change requests
+      // TODO: Mark any floating comments as resolved.
+      // reset review
+      octokit.rest.pulls.requestReviewers({
+        owner: payload.repository.owner.login,
+        repo: payload.repository.name,
+        pull_number: payload.pull_request.number,
+        reviewers: [payload.review.user?.login || ""],
+      });
+    }
+  }
+);
+
+export const webhook = createNodeMiddleware(octoApp.webhooks, { path: "/" });
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,30 @@
-import build from "@hono/vite-build/cloudflare-pages";
-import devServer from "@hono/vite-dev-server";
-import adapter from "@hono/vite-dev-server/cloudflare";
 import { defineConfig } from "vite";
+import { VitePluginNode } from "vite-plugin-node";
 
 export default defineConfig({
   plugins: [
-    build(),
-    devServer({
-      adapter,
-      entry: "src/index.ts",
+    ...VitePluginNode({
+      // Nodejs native Request adapter
+      // currently this plugin support 'express', 'nest', 'koa' and 'fastify' out of box,
+      // you can also pass a function if you are using other frameworks, see Custom Adapter section
+      adapter: ({ app, req, res }) => {
+        app(req, res);
+      },
+      appPath: "./src/index.ts",
+      exportName: "webhook",
+      swcOptions: {
+        jsc: {
+          target: "ESNext",
+          parser: {
+            syntax: "typescript",
+            decorators: true,
+          },
+          transform: {
+            legacyDecorator: true,
+            decoratorMetadata: true,
+          },
+        },
+      },
     }),
   ],
 });
